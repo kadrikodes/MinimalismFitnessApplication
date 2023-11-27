8df809a684863c35f5a0f2d6accd2f269fdf7adc
Samuel Badru <samuelbadru@outlook.com>
2023-11-27 14:46:44 +0000

    private fields made

--- src/main/java/com/health/MinimalismFitnessApp/entities/NutritionTracker.java
+++ src/main/java/com/health/MinimalismFitnessApp/entities/NutritionTracker.java
@@ -1,6 +1,27 @@
 package com.health.MinimalismFitnessApp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class NutritionTracker {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String foodName;
+    private int calories;
+    private int protein;
+    private int carbohydrates;
+    private int fats;
+    private String mealType;
+    private User user;
+
+
+
+
 
 
 
