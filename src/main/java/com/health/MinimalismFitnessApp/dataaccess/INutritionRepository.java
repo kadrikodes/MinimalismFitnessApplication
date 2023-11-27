632eddeb2b7df2cc4e4fd86d56a6f86d70756cc4
Samuel Badru <samuelbadru@outlook.com>
2023-11-27 16:26:08 +0000

    repo extends listcrudrepo

--- src/main/java/com/health/MinimalismFitnessApp/dataaccess/INutritionRepository.java
+++ src/main/java/com/health/MinimalismFitnessApp/dataaccess/INutritionRepository.java
@@ -1,9 +1,10 @@
 package com.health.MinimalismFitnessApp.dataaccess;
 
 import com.health.MinimalismFitnessApp.entities.NutritionTracker;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 
-public interface INutritionRepository {
+public interface INutritionRepository extends ListCrudRepository<NutritionTracker, Long> {
     List<NutritionTracker> findNutritionDataByName(String name);
 }
--- src/main/java/com/health/MinimalismFitnessApp/services/NutritionService.java
+++ src/main/java/com/health/MinimalismFitnessApp/services/NutritionService.java
@@ -1,4 +1,18 @@
 package com.health.MinimalismFitnessApp.services;
 
+import com.health.MinimalismFitnessApp.dataaccess.INutritionRepository;
+import com.health.MinimalismFitnessApp.dataaccess.IUserRepository;
+import com.health.MinimalismFitnessApp.entities.NutritionTracker;
+
+import java.util.List;
+
 public class NutritionService {
+    INutritionRepository nutritionRepository;
+    IUserRepository userRepository;
+
+    public List<NutritionTracker> findAll(){ return nutritionRepository.finda}
+
+
+
+
 }
