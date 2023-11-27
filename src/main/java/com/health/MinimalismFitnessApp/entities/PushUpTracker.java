2f55b5435378608121f1bda132eebfbb392d14d8
RaisGTK <136174485+RaisGTK@users.noreply.github.com>
2023-11-27 14:59:21 +0000

    Entity update

--- src/main/java/com/health/MinimalismFitnessApp/entities/PushUpTracker.java
+++ src/main/java/com/health/MinimalismFitnessApp/entities/PushUpTracker.java
@@ -1,4 +1,50 @@
 package com.health.MinimalismFitnessApp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class PushUpTracker {
+
+    @Id
+    @GeneratedValue
+    private int numberOfPushUps;
+    private int target;
+    private double timeDuration;
+    private int caloriesBurnt;
+
+    public PushUpTracker() {}
+
+    public int getNumberOfPushUps() {
+        return numberOfPushUps;
+    }
+
+    public void setNumberOfPushUps(int numberOfPushUps) {
+        this.numberOfPushUps = numberOfPushUps;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
+    }
+
+    public double getTimeDuration() {
+        return timeDuration;
+    }
+
+    public void setTimeDuration(double timeDuration) {
+        this.timeDuration = timeDuration;
+    }
+
+    public int getCaloriesBurnt() {
+        return caloriesBurnt;
+    }
+
+    public void setCaloriesBurnt(int caloriesBurnt) {
+        this.caloriesBurnt = caloriesBurnt;
+    }
 }
