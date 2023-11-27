36d355951d041896c5c9e13e9d39762ce604da61
Samuel Badru <samuelbadru@outlook.com>
2023-11-27 16:20:16 +0000

    Nutrition Repo method

--- src/main/java/com/health/MinimalismFitnessApp/dataaccess/INutritionRepository.java
+++ src/main/java/com/health/MinimalismFitnessApp/dataaccess/INutritionRepository.java
@@ -1,4 +1,9 @@
 package com.health.MinimalismFitnessApp.dataaccess;
 
+import com.health.MinimalismFitnessApp.entities.NutritionTracker;
+
+import java.util.List;
+
 public interface INutritionRepository {
+    List<NutritionTracker> findNutritionDataByName(String name);
 }
