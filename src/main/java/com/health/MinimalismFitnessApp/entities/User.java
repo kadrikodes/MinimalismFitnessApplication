f89e83c7a06d98791a5d732e420526692b4ddffe
Esra <esra.salem98@gmail.com>
2023-11-27 14:55:45 +0000

    Creating User Class within Entities

--- src/main/java/com/health/MinimalismFitnessApp/entities/User.java
+++ src/main/java/com/health/MinimalismFitnessApp/entities/User.java
@@ -1,4 +1,74 @@
 package com.health.MinimalismFitnessApp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.time.LocalDate;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    public String name;
+    public Long id;
+    public double height;
+    public double weight;
+    public LocalDate birthdate;
+    public String gender;
+
+    @OneToMany //TODO : add this mapping + ArrayList
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public User(){}
+
+
 }
