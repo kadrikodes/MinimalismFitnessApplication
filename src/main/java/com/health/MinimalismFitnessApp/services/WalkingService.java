8b35326be3c17b0d1d3f3ea529c3a2395c996e96
User1 <kadriidris41@yahoo.com>
2023-11-27 16:36:00 +0000

    added a constructor to WalkingTracker class extended IWalkingRepository to CRUD Created skeleton methods on WalkingService

--- src/main/java/com/health/MinimalismFitnessApp/services/WalkingService.java
+++ src/main/java/com/health/MinimalismFitnessApp/services/WalkingService.java
@@ -1,4 +1,31 @@
 package com.health.MinimalismFitnessApp.services;
 
+import com.health.MinimalismFitnessApp.dataaccess.IWalkingRepository;
+import com.health.MinimalismFitnessApp.entities.WalkingTracker;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class WalkingService {
+
+    private IWalkingRepository walkingRepository;
+
+    public List<WalkingTracker> findAll() {
+        return walkingRepository.findAll();
+    }
+
+    public WalkingTracker getWalkingTrackerById(long walkingTrackerId) {
+        Optional<WalkingTracker> walkingTracker = walkingRepository.findById(walkingTrackerId);
+        return walkingTracker.orElse(null);
+    }
+
+    public java.util.List<WalkingTracker> getWalkingTrackerByUserName(String name) {
+        return walkingRepository.findWalkingTrackerByUserName(name);
+    }
+
+    public WalkingTracker addWalkingTrackerData(WalkingTracker walkingTracker) {
+        return walkingTracker;
+    }
 }
