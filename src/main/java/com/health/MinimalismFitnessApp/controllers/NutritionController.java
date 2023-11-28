8419c9d9ef9f7f56659923735b87c62f3a1980fb
Samuel Badru <samuelbadru@outlook.com>
2023-11-28 10:26:25 +0000

    getAllNutritionData get request implemented

--- src/main/java/com/health/MinimalismFitnessApp/controllers/NutritionController.java
+++ src/main/java/com/health/MinimalismFitnessApp/controllers/NutritionController.java
@@ -1,4 +1,27 @@
 package com.health.MinimalismFitnessApp.controllers;
 
+import com.health.MinimalismFitnessApp.entities.NutritionData;
+import com.health.MinimalismFitnessApp.services.NutritionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/nutrition")
 public class NutritionController {
+
+    NutritionService nutritionService;
+
+    @Autowired
+    public NutritionController(NutritionService nutritionService) { this.nutritionService = nutritionService; }
+
+    @GetMapping("")
+    public List<NutritionData> getAllNutritionData() { return nutritionService.findAll();}
+
+
+
+
 }
