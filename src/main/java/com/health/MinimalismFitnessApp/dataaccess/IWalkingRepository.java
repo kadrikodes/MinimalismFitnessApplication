6aff1feb4990d84a9665e18cd4957b73e70abf42
User1 <kadriidris41@yahoo.com>
2023-11-28 11:26:14 +0000

    added more CRUD functionalities which meant, Controller, Service and Repo classes got updated

--- src/main/java/com/health/MinimalismFitnessApp/dataaccess/IWalkingRepository.java
+++ src/main/java/com/health/MinimalismFitnessApp/dataaccess/IWalkingRepository.java
@@ -3,8 +3,13 @@ package com.health.MinimalismFitnessApp.dataaccess;
 import com.health.MinimalismFitnessApp.entities.WalkingData;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IWalkingRepository extends ListCrudRepository<WalkingData, Long> {
     List<WalkingData> findWalkingTrackerByUserName(String name);
+
+    List<WalkingData> findByDateTimeAndDistance(LocalDateTime dateTime, double distance);
+
+    List<WalkingData> findAllByOrderByDateAsc();
 }
