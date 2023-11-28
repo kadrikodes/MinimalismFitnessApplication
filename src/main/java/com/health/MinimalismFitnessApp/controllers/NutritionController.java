a36f1e22de810112cbd7b285a9c1e11d0cd69399
Samuel Badru <samuelbadru@outlook.com>
2023-11-28 10:31:10 +0000

    getAllNutritionDataByID get request implemented

--- src/main/java/com/health/MinimalismFitnessApp/controllers/NutritionController.java
+++ src/main/java/com/health/MinimalismFitnessApp/controllers/NutritionController.java
@@ -3,9 +3,12 @@ package com.health.MinimalismFitnessApp.controllers;
 import com.health.MinimalismFitnessApp.entities.NutritionData;
 import com.health.MinimalismFitnessApp.services.NutritionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,6 +23,15 @@ public class NutritionController {
 
     @GetMapping("")
     public List<NutritionData> getAllNutritionData() { return nutritionService.findAll();}
+
+    @GetMapping("/{nutritionID")
+    public NutritionData getNutritionDataByID(@PathVariable long nutritionID){
+        NutritionData nutritionData = nutritionService.getNutritionID(nutritionID);
+        if(nutritionData == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Nutrition data not found");}
+
+        return nutritionData;
+    }
 
 
 
