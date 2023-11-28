13a7cfb5090e1a1a9460b6d7774ba8dac07c297f
Samuel Badru <samuelbadru@outlook.com>
2023-11-28 10:38:11 +0000

    addNutritionData post request implemented

--- src/main/java/com/health/MinimalismFitnessApp/controllers/NutritionController.java
+++ src/main/java/com/health/MinimalismFitnessApp/controllers/NutritionController.java
@@ -4,10 +4,7 @@ import com.health.MinimalismFitnessApp.entities.NutritionData;
 import com.health.MinimalismFitnessApp.services.NutritionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -37,6 +34,11 @@ public class NutritionController {
     @GetMapping("/user/name/{name}")
     public List<NutritionData> getNutritionDataByUserName(@PathVariable String name){
         return nutritionService.getNutritionByName(name);
+    }
+
+    @PostMapping("/addNutritionData")
+    public NutritionData addNutritionData(@RequestBody NutritionData nutritionData){
+        return nutritionService.addNutritionData(nutritionData);
     }
 
 }
