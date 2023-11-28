54873980ee08ec3ce6dc07e279d20d10aa9c9873
User1 <kadriidris41@yahoo.com>
2023-11-28 11:48:16 +0000

    added @Repository to my repo class

--- src/main/java/com/health/MinimalismFitnessApp/dataaccess/IWalkingRepository.java
+++ src/main/java/com/health/MinimalismFitnessApp/dataaccess/IWalkingRepository.java
@@ -2,10 +2,12 @@ package com.health.MinimalismFitnessApp.dataaccess;
 
 import com.health.MinimalismFitnessApp.entities.WalkingData;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface IWalkingRepository extends ListCrudRepository<WalkingData, Long> {
     List<WalkingData> findWalkingTrackerByUserName(String name);
 
