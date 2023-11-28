2b020c64490892acd7d25792d86da19a256faed0
RaisGTK <136174485+RaisGTK@users.noreply.github.com>
2023-11-28 09:47:00 +0000

    added annotations

--- src/main/java/com/health/MinimalismFitnessApp/dataaccess/IPushUpRepository.java
+++ src/main/java/com/health/MinimalismFitnessApp/dataaccess/IPushUpRepository.java
@@ -3,9 +3,10 @@ package com.health.MinimalismFitnessApp.dataaccess;
 
 import com.health.MinimalismFitnessApp.entities.PushUpTracker;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface IPushUpRepository extends ListCrudRepository<PushUpTracker, Long> {
     List<PushUpTracker> findPushUpTrackerByUserName(String name);
 }
