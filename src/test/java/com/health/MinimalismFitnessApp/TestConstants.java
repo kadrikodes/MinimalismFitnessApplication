b53e67379fb2567a3f7a51f920d68c0a41c7ba4c
Samuel Badru <samuelbadru@outlook.com>
2023-11-29 10:40:45 +0000

    camel case in expected nutrition json

--- src/test/java/com/health/MinimalismFitnessApp/TestConstants.java
+++ src/test/java/com/health/MinimalismFitnessApp/TestConstants.java
@@ -3,6 +3,6 @@ package com.health.MinimalismFitnessApp;
 public class TestConstants {
 
     public static final String EXPECTED_NUTRITION_JSON = """
-            [{"id":1000,"food_name":"Pizza","calories":500,"protein":40,"carbohydrates":30,"fats":30,"meal_type":"Lunch"},{"id":2000,"food_name":"Burger","calories":600,"protein":60,"carbohydrates":20,"fats":20,"meal_type":"Dinner"}]""";
+            [{"id":1000,"foodName":"Pizza","calories":500,"protein":40,"carbohydrates":30,"fats":30,"mealType":"Lunch"},{"id":2000,"foodName":"Burger","calories":600,"protein":60,"carbohydrates":20,"fats":20,"mealType":"Dinner"}]""";
 
 }
