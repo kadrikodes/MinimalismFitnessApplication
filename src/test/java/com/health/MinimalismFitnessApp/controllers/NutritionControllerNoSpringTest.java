896fa80929bbb0e76f00126f18ba649819b6718f
Samuel Badru <samuelbadru@outlook.com>
2023-11-30 15:53:45 +0000

    tests for adding nutrition record and updating in the controller

--- src/test/java/com/health/MinimalismFitnessApp/controllers/NutritionControllerNoSpringTest.java
+++ src/test/java/com/health/MinimalismFitnessApp/controllers/NutritionControllerNoSpringTest.java
@@ -6,6 +6,7 @@ import com.health.MinimalismFitnessApp.services.NutritionService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -31,8 +32,24 @@ public class NutritionControllerNoSpringTest {
     void getNutritonRecordById(){
         when(mockNutritionService.getNutritionID(1000L)).thenReturn(new NutritionData(1000L, "Pounded Yam", 600, 20, 60, 20, "Dinner", new UserData()));
         nutritionController.getNutritionDataByID(1000L);
-
         verify(mockNutritionService, times(1)).getNutritionID(1000L);
+    }
+
+    @Test
+    void addingANutritionRecord() throws URISyntaxException{
+        NutritionData nutritionData = new NutritionData(1000L, "Pounded Yam", 600, 20, 60, 20, "Dinner", new UserData());
+        nutritionController.addNutritionData(nutritionData);
+        verify(mockNutritionService, times(1)).addNutritionData(nutritionData);
+    }
+
+    @Test
+    void updatingNutritionRecord() throws URISyntaxException{
+        NutritionData nutritionData = new NutritionData(1000L, "Pounded Yam", 600, 20, 60, 20, "Dinner", new UserData());
+        nutritionController.addNutritionData(nutritionData);
+        NutritionData updatedNutritionData = new NutritionData(1000L, "Burger", 600, 50, 30, 20, "Lunch", new UserData());
+        nutritionController.updateNutritionData(1000L, updatedNutritionData);
+        verify(mockNutritionService, times(1)).addNutritionData(nutritionData);
+        verify(mockNutritionService, times(1)).updateNutritionData(1000L, updatedNutritionData);
     }
 
 
