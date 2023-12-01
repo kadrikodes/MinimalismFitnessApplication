89449d6e83aacec53987518f9d4a306a4738aa6c
Divin Joseph <101226867+Divin969@users.noreply.github.com>
2023-12-01 11:36:48 +0000

    Final Code for Capstone 2

--- src/main/java/com/health/MinimalismFitnessApp/services/SleepService.java
+++ src/main/java/com/health/MinimalismFitnessApp/services/SleepService.java
@@ -14,8 +14,6 @@ import java.util.Optional;
 
 @Service
 public class SleepService {
-    public static final String SENDER_MUST_EXIST = "The sender of the message must already exist.";
-    public static final String MESSAGE_ID_MUST_BE_NULL_OR_0 = "The message id provided for a create/post must be null or zero.";
 
     ISleepRepository sleepRepository;
     IUserRepository userRepository;
