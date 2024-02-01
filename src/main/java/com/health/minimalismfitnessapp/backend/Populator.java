8950a8018a7bb5d4cad165e74f29264f6775d98b
Divin Joseph <101226867+Divin969@users.noreply.github.com>
2024-02-01 14:02:53 +0000

    .\Added data to all entities

--- src/main/java/com/health/minimalismfitnessapp/backend/Populator.java
+++ src/main/java/com/health/minimalismfitnessapp/backend/Populator.java
@@ -1,14 +1,13 @@
 package com.health.minimalismfitnessapp.backend;
 
 import com.health.minimalismfitnessapp.backend.dataaccess.*;
-import com.health.minimalismfitnessapp.backend.entities.ActivityData;
-import com.health.minimalismfitnessapp.backend.entities.NutritionData;
-import com.health.minimalismfitnessapp.backend.entities.PushUpData;
-import com.health.minimalismfitnessapp.backend.entities.UserData;
+import com.health.minimalismfitnessapp.backend.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Optional;
 
 @Component
@@ -19,6 +18,8 @@ public class Populator {
     IUserRepository iUserRepository;
     IWalkingRepository iWalkingRepository;
     IActivityRepository iActivityRepository;
+    UserData userData;
+    ActivityData activityData;
 
     @Autowired
     public Populator(IActivityRepository iActivityRepository, INutritionRepository iNutritionRepository, IPushUpRepository iPushUpRepository, ISleepRepository iSleepRepository, IUserRepository iUserRepository, IWalkingRepository iWalkingRepository) {
@@ -31,55 +32,71 @@ public class Populator {
     }
 
     public void populate(){
-        UserData userData = new UserData("Rais", 180, 85, LocalDate.of(2000,1,1), "MALE");
-        iUserRepository.save(userData);
-        userData = new UserData("Divin", 160, 68, LocalDate.of(1999,1,1), "MALE");
-        iUserRepository.save(userData);
-        ActivityData activityData = new ActivityData("PushUps", 50);
-        iActivityRepository.save(activityData);
-        PushUpData pushUpData = new PushUpData(20,25,5,50,userData, activityData);
-        iPushUpRepository.save(pushUpData);
+        populateActivityData();
+        populateUserData();
+        populatePushUpData();
+        populateNutritionData();
+        populateWalkingData();
+
     }
 
     public void populateNutritionData() {
-        Optional<UserData> user1Optional = iUserRepository.findById(1L);
-        Optional<UserData> user2Optional = iUserRepository.findById(2L);
-
-        System.out.println("User 1 exists: " + user1Optional.isPresent());
-        System.out.println("User 2 exists: " + user2Optional.isPresent());
-
-        if (user1Optional.isPresent() && user2Optional.isPresent()) {
-            UserData user1 = user1Optional.get();
-            UserData user2 = user2Optional.get();
-
-            NutritionData nutritionData1 = new NutritionData("Pizza", 500, 40, 30, 30, "Lunch", user1);
+            NutritionData nutritionData1 = new NutritionData("Pizza", 500, 40, 30, 30, "Lunch", userData);
             iNutritionRepository.save(nutritionData1);
 
-            NutritionData nutritionData2 = new NutritionData("Burger", 600, 60, 20, 20, "Dinner", user2);
+            NutritionData nutritionData2 = new NutritionData("Burger", 600, 60, 20, 20, "Dinner", userData);
             iNutritionRepository.save(nutritionData2);
-        } else {
-            System.out.println("User with ID 1 or 2 not found");
-            // Add the following logs to see the content of the repository
-            iUserRepository.findAll().forEach(user -> System.out.println("User in repository: " + user));
-        }
     }
 
 
 
     public void populatePushUpData() {
+        PushUpData pushUpData = new PushUpData(20,25,5,50,userData, activityData);
+        iPushUpRepository.save(pushUpData);
+        pushUpData = new PushUpData(25,30,7,70,userData, activityData);
+        iPushUpRepository.save(pushUpData);
+        pushUpData = new PushUpData(27,30,10,90,userData, activityData);
+        iPushUpRepository.save(pushUpData);
 
     }
 
     public void populateSleepData() {
+        SleepData sleepData = new SleepData(LocalTime.of(22,00), LocalTime.of(07,00), LocalTime.of(22, 30), LocalTime.of(07,30, 15), userData);
+        iSleepRepository.save(sleepData);
+        sleepData = new SleepData(LocalTime.of(22,00), LocalTime.of(07,00), LocalTime.of(22, 30), LocalTime.of(07,30, 15), userData);
+        iSleepRepository.save(sleepData);
+        sleepData = new SleepData(LocalTime.of(22,00), LocalTime.of(07,00), LocalTime.of(22, 30), LocalTime.of(07,30, 15), userData);
+        iSleepRepository.save(sleepData);
 
     }
 
     public void populateUserData() {
-
+        UserData userData = new UserData("Rais", 180, 85, LocalDate.of(2000,1,1), "MALE");
+        iUserRepository.save(userData);
+        userData = new UserData("Divin", 160, 68, LocalDate.of(1999,1,1), "MALE");
+        iUserRepository.save(userData);
 
     }
 
     public void populateWalkingData() {
+        WalkingData walkingData = new WalkingData(1, 10, 100, 60, 5, LocalDateTime.of(2023, 11, 10, 12, 30), userData);
+        iWalkingRepository.save(walkingData);
+        walkingData = new WalkingData(10, 20, 120, 70, 10, LocalDateTime.of(2021, 10, 15, 12, 30), userData);
+        iWalkingRepository.save(walkingData);
+        walkingData = new WalkingData(1, 30, 150, 80, 15, LocalDateTime.of(2023, 11, 10, 12, 30), userData);
+        iWalkingRepository.save(walkingData);
+
+    }
+
+    public void populateActivityData() {
+        ActivityData activityData = new ActivityData("PushUps", 50);
+        iActivityRepository.save(activityData);
+        activityData = new ActivityData("Walking", 10);
+        iActivityRepository.save(activityData);
+        activityData = new ActivityData("PushUps", 60);
+        iActivityRepository.save(activityData);
+        activityData = new ActivityData("Walking", 20);
+        iActivityRepository.save(activityData);
 
     }
 
