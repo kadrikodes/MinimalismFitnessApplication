e8f1c32585c3f03859f5b01cca1cca6870f90355
RaisGTK <136174485+RaisGTK@users.noreply.github.com>
2024-02-05 13:17:02 +0000

    Navbar complete

--- src/main/java/com/health/minimalismfitnessapp/frontend/fitness-app/src/App.js
+++ src/main/java/com/health/minimalismfitnessapp/frontend/fitness-app/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
-import { Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import NavBar from './components/HomePage/NavBar/NavBar';
+import WalkPage from './components/WalkPage/WalkPage';
+import PushUpPage from './components/PushUpPage/PushUpPage';
+import NutritionPage from './components/NutritionPage/NutritionPage';
+import SleepPage from './components/SleepPage/SleepPage';
+import UserPage from './components/UserPage/UserPage';
+
 
 function App() {
   return (
+    <>
     <Routes>
-        <Route path='/' element={<HomePage />}/>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/user' element={<UserPage />} />
+            <Route path="/walk" element={<WalkPage />} />
+            <Route path="/pushup" element={<PushUpPage />} />
+            <Route path="/nutrition" element={<NutritionPage />} />
+            <Route path="/sleep" element={<SleepPage />} />
     </Routes>
-
+    </>
   );
 }
 
---
+++ src/main/java/com/health/minimalismfitnessapp/frontend/fitness-app/src/components/WalkPage/WalkPage.js
@@ -0,0 +1,7 @@
+
+
+const WalkPage = () => {
+
+}
+
+export default WalkPage;
