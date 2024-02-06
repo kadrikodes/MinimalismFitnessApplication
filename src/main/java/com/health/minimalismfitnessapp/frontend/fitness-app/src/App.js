f0c23d323e08d43264ff93e44b6be8ae79ea2ab7
Samuel Badru <samuelbadru@outlook.com>
2024-02-06 09:53:16 +0000

    Fix import in app.js

--- src/main/java/com/health/minimalismfitnessapp/frontend/fitness-app/src/App.js
+++ src/main/java/com/health/minimalismfitnessapp/frontend/fitness-app/src/App.js
@@ -2,14 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
 
-import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/HomePage/NavBar/NavBar';
 import WalkPage from './components/WalkPage/WalkPage';
 import PushUpPage from './components/PushUpPage/PushUpPage';
 import NutritionPage from './components/NutritionPage/NutritionPage';
 import SleepPage from './components/SleepPage/SleepPage';
 import UserPage from './components/UserPage/UserPage';
-import { Routes, Route} from 'react-router-dom';
 import LoginPage from './components/RegistrationPage/LoginPage';
 import SignupPage from './components/RegistrationPage/SignUpPage';
 
