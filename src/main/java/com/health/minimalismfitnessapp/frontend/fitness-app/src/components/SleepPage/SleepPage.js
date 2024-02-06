98476d3042e7f6f176d0cad5466218028be3737c
Divin Joseph <101226867+Divin969@users.noreply.github.com>
2024-02-06 11:52:17 +0000

    Background template of sleep page created

--- src/main/java/com/health/minimalismfitnessapp/frontend/fitness-app/src/components/SleepPage/SleepPage.js
+++ src/main/java/com/health/minimalismfitnessapp/frontend/fitness-app/src/components/SleepPage/SleepPage.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import "./SleepPage.css";
+import SleepContainer from "../HomePage/SleepSummary/SleepContainer";
 
 
 const SleepPage = () => {
+    const [sleepData, setSleepData] = useState(null);
 
-}
+    useEffect(() => {
+        const sleepAPI = 'http://localhost:8080/sleeptracker/1';
+
+        fetch(sleepAPI)
+        .then((response) => {return response.json();})
+        .then((data) => {setSleepData(data);})
+
+
+    }, [])
+
+    return (
+        <div className="desktop">
+              <div className="containers">
+                <SleepContainer sleepData={sleepData} />
+              </div>
+          {/* {userData ? (<pre>{JSON.stringify(userData, null, 2)}</pre>) : (<p>Loading...</p>)} */}
+        </div>
+      );
+    };
 
 export default SleepPage;
